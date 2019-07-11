ac04a40c938cb196c37591f173cd1d993195f567
Israel Martinez <marcemc@yahoo.com>
2019-07-11 18:28:16 -0400

    Components without routes

--- src/components/CitySearch.jsx
+++ src/components/CitySearch.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 
 export default class CitySearch extends React.Component {
+
+  handleChange = event => {
+    // console.log( event.target.value )
+    this.props.setCity( event.target.value )
+  }
+
   render(){
     return(
       <form>
-        <label htmlFor=""></label>
+        <label htmlFor="city">City: </label>
+        <input
+          name="city"
+          type="search"
+          placeholder="e.g.: Boston"
+          autoComplete="off"
+          onChange={ this.handleChange }/>
       </form>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-import CitySearch from './components/CitySearch'
+import WeatherApp from './components/WeatherApp'
 
 
-ReactDOM.render(<CitySearch />, document.getElementById('root'));
+ReactDOM.render(<WeatherApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
